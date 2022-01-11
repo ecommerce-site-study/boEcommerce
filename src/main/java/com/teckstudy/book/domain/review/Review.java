7e0bd829f35633ba0f68db3d1fe2ea1736480292
prodo-developer <wjdals300@gmail.com>
2022-01-12 00:29:38 +0900

    refactor : 패키지 이동 및 엔티티 컬럼명 수정

--- src/main/java/com/teckstudy/book/domain/review/Review.java
+++ src/main/java/com/teckstudy/book/domain/review/Review.java
@@ -1,4 +1,4 @@
-package com.teckstudy.book.domain.product;
+package com.teckstudy.book.domain.review;
 
 import com.teckstudy.book.domain.base.BaseEntity;
 import com.teckstudy.book.domain.member.Member;
@@ -18,21 +18,21 @@ public class Review extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long review_sn;
+    private Long reviewId;
 
     @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "member_sn")
-    private Member member;
+    @JoinColumn(name = "memberId")
+    private Member memberId;
 
     @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "product_sn")
-    private Product product;
+    @JoinColumn(name = "productId")
+    private Product productId;
 
     @Column(length = 40, nullable = false)
-    private String review_sub;
+    private String reviewSub;
 
     @Column(columnDefinition = "TEXT", nullable = false)
-    private String review_content;
+    private String reviewContent;
 
     @Column(length = 1, nullable = false)
     private int gpa;
