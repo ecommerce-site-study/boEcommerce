3b22d74871ca5b94bb22dc8ae2f7d26bce536f84
prodo-developer <wjdals300@gmail.com>
2022-01-26 23:32:41 +0900

    refactor : 전시관리 레파지토리 수정기능 비활성화

--- src/main/java/com/teckstudy/book/domain/exhibition/repository/ContentsTypeRepository.java
+++ src/main/java/com/teckstudy/book/domain/exhibition/repository/ContentsTypeRepository.java
@@ -1,18 +1,10 @@
 package com.teckstudy.book.domain.exhibition.repository;
 
 import com.teckstudy.book.domain.exhibition.ContentsType;
-import com.teckstudy.book.domain.exhibition.types.ContentEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContentsTypeRepository extends JpaRepository<ContentsType, Long> {
-
-    @Modifying(clearAutomatically = true)
-    @Query("UPDATE ContentsType p SET p.contentEnum = :contentEnum, p.contentCnt = :contentCnt WHERE p.contentId = :id")
-    void updateContents(@Param("id")Long id, @Param("contentEnum") ContentEnum contentEnum, @Param("contentCnt") int contentCnt);
 
 }
--- src/main/java/com/teckstudy/book/domain/exhibition/repository/ExhibitionRepository.java
+++ src/main/java/com/teckstudy/book/domain/exhibition/repository/ExhibitionRepository.java
@@ -1,12 +1,7 @@
 package com.teckstudy.book.domain.exhibition.repository;
 
-import com.teckstudy.book.application.exhibition.dto.ExhibitionDto;
-import com.teckstudy.book.application.exhibition.dto.SearchExhibitionDto;
 import com.teckstudy.book.domain.exhibition.Exhibition;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -15,12 +10,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ExhibitionRepository extends JpaRepository<Exhibition, Long> {
 
-    @Modifying(clearAutomatically = true)
-    @Query("UPDATE Exhibition p SET p.useYn = :#{#searchExhibitionDto.toEntity().useYn}, p.name = :#{#searchExhibitionDto.toEntity().name}, " +
-            "p.exhibitionType = :#{#searchExhibitionDto.toEntity().exhibitionType}, p.dateYn = :#{#searchExhibitionDto.toEntity().dateYn}, " +
-            "p.image = :#{#searchExhibitionDto.toEntity().image}, p.description = :#{#searchExhibitionDto.toEntity().description}, " +
-            "p.url = :#{#searchExhibitionDto.toEntity().url}, p.exhibitionStart = :#{#searchExhibitionDto.toEntity().exhibitionStart}, " +
-            "p.exhibitionEnd = :#{#searchExhibitionDto.toEntity().exhibitionEnd}, p.bundleContentCnt = :#{#searchExhibitionDto.toEntity().bundleContentCnt} " +
-            "WHERE p.exhibitionId = :id")
-    void updateExhibition(@Param("searchExhibitionDto") SearchExhibitionDto searchExhibitionDto, @Param("id") Long id);
 }
