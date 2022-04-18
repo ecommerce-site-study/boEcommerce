9c9d00d7c3e6c55e99a980abe66f9cf7b8bb442d
YunNote <zzdd1558@gmail.com>
2022-04-18 22:48:17 +0900

    feature: default 이미지 처리 handler 추가

--- src/main/java/com/teckstudy/book/core/configuration/MvcConfigurer.java
+++ src/main/java/com/teckstudy/book/core/configuration/MvcConfigurer.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -24,5 +25,11 @@ public class MvcConfigurer implements WebMvcConfigurer {
                         HttpMethod.DELETE.name())
                 .maxAge(3600)
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/bucket/**")
+                .addResourceLocations("classpath:/static/bucket/", "/static/bucket/");
     }
 }
